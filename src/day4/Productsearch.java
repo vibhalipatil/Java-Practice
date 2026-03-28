81b469f6f0f1ec364fd1bdeda6f4997deb694c9e
vibhalipatil <vibhalipatil.alibag@gmail.com>
2026-03-28 19:48:18 +0530

    FOr loop to search product

---
+++ src/day4/Productsearch.java
@@ -0,0 +1,28 @@
+package day4;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Productsearch {
+
+	public static void main(String[] args) {
+		
+		 List<String> producst = Arrays.asList("Laptop", "Mobile", "Tablet", "Headphones", "Camera");
+
+	        String finalproduct = "Headphones";
+	        boolean productfound = false;
+
+	        for (String product : producst) {
+	            if (product.equals(finalproduct)) {
+	                productfound = true;
+	                break;
+	            }
+	        }
+
+	        if (productfound) {
+	            System.out.println("Product Found ");
+	        } else {
+	            System.out.println("Product Not Found ❌");
+	        }
+	    }
+	}
