986083502b61df1abab5fe11291fb5d8e43d8dc8
vibhalipatil <vibhalipatil.alibag@gmail.com>
2026-04-02 21:57:35 +0530

    Clearing Polymorphism and static variable concept

--- src/day8/LoginExecution.java
+++ src/day8/LoginExecution.java
@@ -4,6 +4,7 @@ public class LoginExecution {
 
 	public static void main(String[] args) {
 		Logindata.launchApp(); // “Calling a static method to internally uses the static variable.
+		System.out.println(Logindata.appURL); //“Calling a static variable directly.
 		
 		Logindata lg = new Logindata("Vibhali" , "Vib19");
 		lg.login("Vibhali" , "Vib19");
